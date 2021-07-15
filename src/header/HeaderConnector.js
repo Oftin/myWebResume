fb2c43b0f998b34190f375c8e3f831d79249c9c3
Kamil Bobrowski <Bobrowski.Kamil96@gmail.com>
2021-07-15 02:35:58 +0200

    Remove prefix 'sm' in HeaderConnector, next idea to fix no working tailwind in mobile site

--- src/header/HeaderConnector.js
+++ src/header/HeaderConnector.js
@@ -10,15 +10,15 @@ export class HeaderConnector extends Component {
                 <div className='relative font-bold font-serif place-items-center '>
                     <img className='relative z-0 w-screen' src={ imgHeader } alt=''/>
                     <div className='absolute z-10 text-2xl bottom-5 left-6 text-coolGray-400'>
-                        <h1 className='sm:text-4xl md:text-5xl ld:text-5xl text-yellow-500'>Kamil Bobrowski</h1>
+                        <h1 className='text-4xl md:text-5xl ld:text-5xl text-yellow-500'>Kamil Bobrowski</h1>
                         <img className='absolute z-10 top-0 -right-1/4 h-11' src={ flag } alt='' />
-                        <div className='font-mono sm:text-xl md:text-2xl ld:text-2xl '>Engineer / Artist / Combinator</div>
+                        <div className='font-mono text-xl md:text-2xl ld:text-2xl '>Engineer / Artist / Combinator</div>
                     </div>
                 </div>
-                <div className='flex place-content-center h-16 sm:m-16 md:m-18 ld:m-20'>
+                <div className='flex place-content-center h-16 m-16 md:m-18 ld:m-20'>
                     <div className='justify-center italic font-normal text-xl'>
                         <div className=''>"{ Data[8].quort }"</div>
-                        <div className='flex justify-center sm:mt-2 md:mt-2 ld:mt-2'> - { Data[8].author } </div>
+                        <div className='flex justify-center mt-2 md:mt-2 ld:mt-2'> - { Data[8].author } </div>
                     </div>
                 </div>
             </Fragment>
